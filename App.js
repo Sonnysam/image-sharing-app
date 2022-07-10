e741f6d726c8ca022a5b58e3ad723c9dc9c698df
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2022-07-10 02:35:18 +0000

    expo img and sharing picker added

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { logo } from './assets/logo.png';
-
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { logo } from "./assets/logo.png";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+    let openImagePickerAsync = async () => {
+      let permissionResult =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+      if (permissionResult.granted === false) {
+        alert("Permission to access camera roll is required!");
+        return;
+      }
+
+      let pickerResult = await ImagePicker.launchImageLibraryAsync();
+      // console.log(pickerResult);
+         if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  
+    };
+  
   return (
     <View style={styles.container}>
       <Image
         // source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
-        source={require('./assets/logo.png')}
+        source={require("./assets/logo.png")}
         style={styles.logo}
       />
-
+      ;
       <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -30,11 +63,21 @@ const styles = StyleSheet.create({
   logo: {
     width: 305,
     height: 159,
-    marginBottom: 10,
+    marginBottom: 20,
   },
   instructions: {
     color: "#888",
     fontSize: 18,
     marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
