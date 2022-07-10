5022b5d14284632fe58c5199e758f01073f0cad0
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2022-07-10 02:13:00 +0000

    logo added

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={{ width: 305, height: 159 }} />
+
+      <Text style={{ color: "#888", fontSize: 18 }}>
+        To share a photo from your phone with a friend, just press the button
+        below!
+      </Text>
     </View>
   );
 }
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
