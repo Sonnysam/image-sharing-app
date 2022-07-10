871d2cafed0b5046eae74d49010c37c85c01ead0
Samuel Agbenyo <80902426+Sonnysam@users.noreply.github.com>
2022-07-10 02:20:27 +0000

    logo added

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
-import logo from "./assets/logo.png";
+import { logo } from './assets/logo.png';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={logo} style={{ width: 305, height: 159 }} />
+      <Image
+        // source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+        source={require('./assets/logo.png')}
+        style={styles.logo}
+      />
 
-      <Text style={{ color: "#888", fontSize: 18 }}>
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
@@ -21,5 +26,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: "#888",
+    fontSize: 18,
+    marginHorizontal: 15,
   },
 });
